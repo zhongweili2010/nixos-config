8f423105c196e99bec8d2d5bc991c089f77639da
zhongwei li <zhongweili2010@gmail.com>
2024-07-10 21:21:54 -0700

    added mac

--- flake.nix
+++ flake.nix
@@ -9,10 +9,23 @@
 
   outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      pc = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./nixos/hardware-configuration.nix
+          ./nixos/hardware-pc.nix
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.alex = import ./home-manager/home.nix;
+          }
+        ];
+      };
+      mac = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/hardware-mac.nix
           ./nixos/configuration.nix
           home-manager.nixosModules.home-manager
           {
